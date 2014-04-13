674bbcf301e89a351b0fddacc914e8e2b5c1207b
Vincent RAMPAL <vincent.rampal@gmail.com>
2014-04-13 14:22:25 +0200

    Moved code from JSP to Java

--- webjsp/src/main/java/vrampal/connectfour/servlet/ConnectFourHelper.java
+++ webjsp/src/main/java/vrampal/connectfour/servlet/ConnectFourHelper.java
@@ -3,6 +3,7 @@ package vrampal.connectfour.servlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import vrampal.connectfour.core.Board;
 import vrampal.connectfour.core.ConnectFourException;
 import vrampal.connectfour.core.Game;
 import vrampal.connectfour.core.GameStatus;
@@ -15,12 +16,16 @@ public class ConnectFourHelper {
 
   private Game game = null;
 
+  private String mainMessage = "";
+
   private String subMessage = "";
 
   public void pageBegin(HttpServletRequest req) {
 
     HttpSession session = req.getSession();
+
     game = (Game) session.getAttribute(GAME_KEY);
+
     String resetStr = req.getParameter("reset");
     if ((game == null) || (resetStr != null)) {
       // TODO use dependency injection
@@ -40,27 +45,64 @@ public class ConnectFourHelper {
         subMessage = e.getMessage();
       }
     }
+
+    if (game.getStatus() == GameStatus.ONGOING) {
+      Player player = game.getCurrentPlayer();
+      mainMessage = "Now playing: " + player.getName();
+    } else if (game.getStatus() == GameStatus.FINISHED) {
+      Player winner = game.getWinner();
+      if (winner != null) {
+        mainMessage = winner.getName() + " won the game.";
+      } else {
+        mainMessage = "It's a draw game.";
+      }
+    }
   }
 
   public String getMainMessage() {
-    String message = "";
-
-    if (game.getStatus() == GameStatus.FINISHED) {
-      Player winner = game.getWinner();
-      if (winner != null) {
-        message = winner.getName() + " won the game.";
-      } else {
-        message = "It's a draw game.";
-      }
-    } else if (game.getStatus() == GameStatus.ONGOING) {
-      Player player = game.getCurrentPlayer();
-      message = "Now playing: " + player.getName();
-    }
-    return message;
+    return mainMessage;
   }
 
   public String getSubMessage() {
     return subMessage;
+  }
+
+  public String printBoard() {
+    StringBuilder buff = new StringBuilder(1300);
+
+    Board board = game.getBoard();
+    int height = board.getHeight();
+    int width = board.getWidth();
+
+    if (game.getStatus() == GameStatus.ONGOING) {
+      buff.append("<tr>");
+      for (int colIdx = 1; colIdx <= width; colIdx++) {
+        buff.append("<td class=\"cfh\"><a href=\"?col=");
+        buff.append(colIdx);
+        buff.append("\">V</a></td>");
+      }
+      buff.append("</tr>");
+    }
+
+    for (int rowIdx = height; rowIdx > 0; rowIdx--) {
+      buff.append("<tr>");
+      for (int colIdx = 1; colIdx <= width; colIdx++) {
+        Player content = board.getCell(colIdx - 1, rowIdx - 1);
+        char c = content.getLetter();
+        String clazz = "cfe";
+        if (c == 'R') {
+          clazz = "cfr";
+        } else if (c == 'Y') {
+          clazz = "cfy";
+        }
+        buff.append("<td class=\"");
+        buff.append(clazz);
+        buff.append("\"></td>");
+      }
+      buff.append("</tr>");
+    }
+
+    return buff.toString();
   }
 
   public Game getGame() {
--- webjsp/src/main/webapp/index.jsp
+++ webjsp/src/main/webapp/index.jsp
@@ -1,7 +1,5 @@
 <jsp:directive.page contentType="text/html" pageEncoding="UTF-8" />
-<jsp:useBean id="helper"
-	class="vrampal.connectfour.servlet.ConnectFourHelper" scope="page" />
-<jsp:directive.page import="vrampal.connectfour.core.*" />
+<jsp:useBean id="helper" class="vrampal.connectfour.servlet.ConnectFourHelper" scope="page" />
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,50 +8,16 @@
 <link rel="stylesheet" href="connectfour.css" type="text/css" media="screen">
 </head>
 <body>
-	<%
-	  helper.pageBegin(request);
-	  Game game = helper.getGame();
-	%>
-	<h2><%=helper.getMainMessage()%></h2>
-	<h2><%=helper.getSubMessage()%></h2>
-	<p>Game id: <%=game.getId()%></p>
-	<table class="cfb">
-		<tbody>
-			<%
-			  Board board = game.getBoard();
-			  int height = board.getHeight();
-			  int width = board.getWidth();
-
-			  if (game.getStatus() == GameStatus.ONGOING) {
-			    out.print("<tr>");
-			    for (int colIdx = 1; colIdx <= width; colIdx++) {
-			      out.print("<td class=\"cfh\"><a href=\"index.jsp?col=");
-			      out.print(colIdx);
-			      out.print("\">V</a></td>");
-			    }
-			    out.print("</tr>");
-			  }
-
-			  for (int rowIdx = height; rowIdx > 0; rowIdx--) {
-			    out.print("<tr>");
-			    for (int colIdx = 1; colIdx <= width; colIdx++) {
-			      Player content = board.getCell(colIdx - 1, rowIdx - 1);
-			      char c = content.getLetter();
-			      String clazz = "cfe";
-			      if (c == 'R') {
-			        clazz = "cfr";
-			      } else if (c == 'Y') {
-			        clazz = "cfy";
-			      }
-			      out.print("<td class=\"");
-			      out.print(clazz);
-			      out.print("\"></td>");
-			    }
-			    out.print("</tr>");
-			  }
-			%>
-		</tbody>
-	</table>
-	<p><a href="index.jsp?reset=1">Play another game</a></p>
+<%
+  helper.pageBegin(request);
+  Game game = helper.getGame();
+%>
+<h2><%=helper.getMainMessage()%></h2>
+<h3><%=helper.getSubMessage()%></h3>
+<p>Game id: <%=game.getId()%></p>
+<table class="cfb"><tbody>
+<%=helper.printBoard()%>
+</tbody></table>
+<p><a href="?reset=1">Play another game</a></p>
 </body>
 </html>
